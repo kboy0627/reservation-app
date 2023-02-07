d1c8cb99cc04057e675f4aba7b149eed3037790d
kboy0627 <k.k.9201.baaser@gmail.com>
2023-02-07 23:28:00 +0900

    ヘッダーコンポーネント追加

---
+++ src/app/common/navber/navber.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navber',
+  templateUrl: './navber.component.html',
+  styleUrls: ['./navber.component.scss']
+})
+export class NavberComponent {
+}
